60892c4d38711812b36897f155b935729fd8d983
范帆 <fanfan@fanfandeMacBook-Pro.local>
2019-11-08 17:33:27 +0800

    commit

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int n;
+    cin>>n;
+    for(int i=0;i<10;++i)
+    {
+        printf("%04d\n",i);
+    }
+    return 0;
+}
